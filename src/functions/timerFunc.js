99c0d197a6d3d17fa4090db19b937b3def0b9d92
Yash Girme <yashgirme08@gmail.com>
2026-03-22 15:07:20 +0530

    Delete src/functions/timerFunc.js

--- src/functions/timerFunc.js
+++
@@ -1,8 +0,0 @@
-const { app } = require('@azure/functions');
-
-app.timer('timerFunc', {
-    schedule: '0 * * * * *',
-    handler: (myTimer, context) => {
-        context.log('Timer function processed request.');
-    }
-});
